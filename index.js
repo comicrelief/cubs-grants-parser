9672b9023ee007bbc9bc667f533c6716071af9ba
Peter Vanhee <peter.vanhee@gmail.com>
2017-06-09 14:40:51 +0100

    add eslint and fix coding standards

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-'use strict';
-
-var request = require('request');
+const request = require('request');
 
 module.exports = {
 
-	parseGrants: function(callback) {
-		var method = 'GetLiveGrantsProjects';
-		// var method = 'GetCountries';
-		var url = process.env.CUBS_URL + '/' + method;
+  parseGrants(callback) {
+    const method = 'GetLiveGrantsProjects';
+    // const method = 'GetCountries';
+    const url = process.env.CUBS_URL + '/' + method;
 
-		// put in request with promises
-		var qs = {
-			Username: process.env.CUBS_USERNAME,
-			Salt: process.env.CUBS_SALT,
-			SecurityKey: process.env.CUBS_SECURITY_KEY
-		};
+    // put in request with promises
+    const qs = {
+      Username: process.env.CUBS_USERNAME,
+      Salt: process.env.CUBS_SALT,
+      SecurityKey: process.env.CUBS_SECURITY_KEY,
+    };
 
-		request.get({
-			url: url,
-			qs: qs
-		}, function(err, response, body) {
-
-			// Do some data cleanup
-			var body = body
+    request.get({
+      url,
+      qs,
+    }, (err, response, body) => {
+      // Do some data cleanup
+      let data = body
         .replace('<?xml version="1.0" encoding="utf-8"?>', '')
         .replace('<string xmlns="http://tempuri.org/">', '')
         .replace('</string>', '')
         .replace(/\\\\"/gi, '');
 
-      var json = JSON.parse(body);
-      return callback(json.GrantsProjects.GrantsProject);
+      data = JSON.parse(data);
+      return callback(data.GrantsProjects.GrantsProject);
     });
-	}
+  },
 
 };
--- test/test.js
+++ test/test.js
@@ -1,34 +1,29 @@
-'use strict';
-
-var chai = require('chai');
-var expect = chai.expect;
-var cubs = require('../index');
-var env = require('node-env-file');
+const chai = require('chai');
+const cubs = require('../index');
+// const env = require('node-env-file');
+const expect = chai.expect;
 
 chai.should();
 chai.use(require('chai-things'));
 
 // env(__dirname + '/../.env');
 
-describe('CUBS Parser', function() {
-
-  it('should have access to environment variables for secrets management', function() {
+describe('CUBS Parser', () => {
+  it('should have access to environment variables for secrets management', () => {
     expect(process.env.CUBS_URL).to.not.be.an('undefined', 'CUBS_URL not set');
     expect(process.env.CUBS_USERNAME).to.not.be.an('undefined', 'CUBS_USERNAME not set');
     expect(process.env.CUBS_SALT).to.not.be.an('undefined', 'CUBS_SALT not set');
     expect(process.env.CUBS_SECURITY_KEY).to.not.be.an('undefined', 'CUBS_SECURITY_KEY not set');
   });
 
-  it('should parse grants data', function(done) {
-
+  it('should parse grants data', (done) => {
     // Use a higher timeout to fetch the remote file
     this.timeout(90000);
-
-    cubs.parseGrants(function(grants) {
+    cubs.parseGrants((grants) => {
       expect(grants).to.be.an('array').and.to.have.lengthOf.at.least(1);
       expect(grants[0]).to.be.an('object');
       grants.should.all.have.property('AmountAwarded');
-    	grants.should.all.have.property('CopywrittenSummary');
+      grants.should.all.have.property('CopywrittenSummary');
       grants.should.all.have.property('GrantsProjectID');
       grants.should.all.have.property('Issue');
       grants.should.all.have.property('IssueID');
