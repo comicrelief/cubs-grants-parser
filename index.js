4362cebe6188926e7983a2b3ec04a103ed64e62d
Peter Vanhee <peter.vanhee@gmail.com>
2017-06-15 15:25:26 +0100

    add countries support (#2)
    * restructure npm
    
    * parse countries method with test
    
    * add parseGrantsByCountry method
    
    * add grants with countries callback
    
    * annotate grants with country and detailed info from other callbacks
    
    * Add country support now part of method
    
    * remove convoluted countries parsing method, not needed anymore

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const request = require('request');
+const env = require('node-env-file');
+
+// Load API credentials from a local environment file if available
+env(__dirname + '/.env', { raise: false });
+
+// put in request with promises
+const qs = {
+  Username: process.env.CUBS_USERNAME,
+  Salt: process.env.CUBS_SALT,
+  SecurityKey: process.env.CUBS_SECURITY_KEY,
+};
+
+constructUrl = (method) => {
+  return process.env.CUBS_URL + '/' + method;
+};
+
+cleanupJson = (data) => {
+  const json = data
+    .replace('<?xml version="1.0" encoding="utf-8"?>', '')
+    .replace('<string xmlns="http://tempuri.org/">', '')
+    .replace('</string>', '')
+    .replace(/\\\\"/gi, '');
+  return JSON.parse(json);
+};
 
 module.exports = {
 
-  parseGrants() {
-    const method = 'GetLiveGrantsProjects';
-    // const method = 'GetCountries';
-    const url = process.env.CUBS_URL + '/' + method;
+  parseCountries() {
+    const url = constructUrl('GetCountries');
 
-    // put in request with promises
-    const qs = {
-      Username: process.env.CUBS_USERNAME,
-      Salt: process.env.CUBS_SALT,
-      SecurityKey: process.env.CUBS_SECURITY_KEY,
-    };
 
     return new Promise((resolve, reject) => {
       request.get({
@@ -23,15 +39,55 @@ module.exports = {
           reject(err);
         }
 
-        // Do some data cleanup
-        let data = body
-          .replace('<?xml version="1.0" encoding="utf-8"?>', '')
-          .replace('<string xmlns="http://tempuri.org/">', '')
-          .replace('</string>', '')
-          .replace(/\\\\"/gi, '');
+        const data = cleanupJson(body);
+        resolve(data.Countries.Country);
+      });
+    });
+  },
 
-        data = JSON.parse(data);
-        resolve(data.GrantsProjects.GrantsProject);
+  parseGrantsByCountry(countryID) {
+    const url = constructUrl('GetGrantsProjectsByCountry');
+
+    const qs2 = qs;
+    qs2.CountryID = countryID;
+
+    return new Promise((resolve, reject) => {
+      request.get({
+        url,
+        qs: qs2,
+      }, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+
+        let data = cleanupJson(body);
+        data = data.GrantsProjects.GrantsProject;
+
+        // Wrap into array as some countries only return a single grant
+        if (!(data instanceof Array)) {
+          data = [data];
+        }
+
+        resolve(data);
+      });
+    });
+  },
+
+  parseGrants() {
+    const url = constructUrl('GetLiveGrantsProjects');
+
+    return new Promise((resolve, reject) => {
+      request.get({
+        url,
+        qs,
+      }, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+
+        let data = cleanupJson(body);
+        data = data.GrantsProjects.GrantsProject;
+        resolve(data);
       });
     });
   },
--- test/test.js
+++ test/test.js
@@ -1,22 +1,52 @@
 const chai = require('chai');
 const cubs = require('../index');
-const env = require('node-env-file');
 const expect = chai.expect;
 
 chai.should();
 chai.use(require('chai-things'));
 
 describe('CUBS Parser', function() {
-  before(function() {
-    // Load API credentials from a local environment file if available
-    env(__dirname + '/../.env', {raise: false});
-  });
 
   it('should have access to environment variables for secrets management', function() {
     expect(process.env.CUBS_URL).to.not.be.an('undefined', 'CUBS_URL not set');
     expect(process.env.CUBS_USERNAME).to.not.be.an('undefined', 'CUBS_USERNAME not set');
     expect(process.env.CUBS_SALT).to.not.be.an('undefined', 'CUBS_SALT not set');
     expect(process.env.CUBS_SECURITY_KEY).to.not.be.an('undefined', 'CUBS_SECURITY_KEY not set');
+  });
+
+  it('should parse countries data', function() {
+    // Use a higher timeout to fetch the remote file
+    this.timeout(90000);
+
+    return cubs.parseCountries()
+      .then(function(countries) {
+        expect(countries).to.be.an('array').and.to.have.lengthOf.at.least(1);
+        expect(countries[0]).to.be.an('object');
+        countries.should.all.have.property('CountryCode');
+        countries.should.all.have.property('CountryID');
+        countries.should.all.have.property('CountryName');
+      });
+  });
+
+  it('should parse grants data by country', function() {
+    // Use a higher timeout to fetch the remote file
+    this.timeout(90000);
+
+    const maliCountryID = 137;
+
+    return cubs.parseGrantsByCountry(maliCountryID)
+      .then(function(grants) {
+        expect(grants).to.be.an('array').and.to.have.lengthOf.at.least(1);
+        expect(grants[0]).to.be.an('object');
+        grants.should.all.have.property('AmountAwarded');
+        grants.should.all.have.property('CopywrittenSummary');
+        grants.should.all.have.property('Country');
+        grants.should.all.have.property('GrantsProjectID');
+        grants.should.all.have.property('Issue');
+        grants.should.all.have.property('IssueID');
+        grants.should.all.have.property('Name');
+        grants.should.all.have.property('StartDate');
+      });
   });
 
   it('should parse grants data', function() {
@@ -39,6 +69,9 @@ describe('CUBS Parser', function() {
         grants.should.all.have.property('Region');
         grants.should.all.have.property('StartDate');
         grants.should.all.have.property('SubRegionID');
+        grants.should.all.have.property('CountryCode');
+        grants.should.all.have.property('CountryID');
+        grants.should.all.have.property('CountryName');
       });
   });
 });
